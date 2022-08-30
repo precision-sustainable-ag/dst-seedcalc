05c1ad773f8f1f0529984dc50fe1a16045d8c843
0101kvmt <0101kvmt@gmail.com>
2022-08-30 08:23:36 -0700

    updated button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,14 +2,26 @@ import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import { dstTheme, dstTheme2 } from "../../shared/themes";
 
-export const DSTButton = ({text, buttonClass, type, size, theme}) => {
+/* 
+
+text: Button text
+buttonClass: Class of button
+size: Size of the Grid container
+theme: Theme of the button
+path: Formatted as {type: 'url/local', path: 'example', url: 'www.example.com'}
+    type: specify whether this is a URL or local path
+    path: local path
+    url: URL string
+*/
+export const DSTButton = ({text, buttonClass, size, theme, path}) => {
     let themeType = theme === 'dstTheme' ? dstTheme : dstTheme2;
     return (
         <Grid xs={size} className={buttonClass}>
             <Button 
                 variant="contained"
                 theme={dstTheme2}
-            ><span>{text}</span></Button>                     
+            ><Link
+            to={path}>{text}</Link></Button>                     
         </Grid>    
     )
 }
