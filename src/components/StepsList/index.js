949d4eccec9c2ec8dc6b52e0bd1a28ebda5a104a
Kevin Tran <0101kvmt@gmail.com>
2022-09-09 11:37:51 -0700

    removed unnecessary imports

--- src/components/StepsList/index.js
+++ src/components/StepsList/index.js
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from "react";
+import { Fragment } from "react";
 import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
