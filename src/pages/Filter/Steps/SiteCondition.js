ca5d5b144c8fc98980afce9fb8ee74681d725fc6
Kevin Tran <0101kvmt@gmail.com>
2022-09-13 08:50:22 -0700

    wrap-up of github issues

--- src/pages/Filter/Steps/SiteCondition.js
+++ src/pages/Filter/Steps/SiteCondition.js
@@ -5,18 +5,21 @@ import { DatePicker } from "../../../components/DatePicker";
 import { Dropdown } from "../../../components/Dropdown";
 import { DSTButton } from "../../../components/Button";
 import { SearchField } from "../../../components/SearchField";
+import { DSTSwitch } from "../../../components/Switch";
 import "./steps.css";
 const SiteCondition = () => {
   const [date, setDate] = useState(dayjs("2014-08-18T21:11:54"));
   const [dropdownValue, setDropdownValue] = useState("test");
-
+  const [checked, handleSwitchChange] = useState(false);
   const handleDatePicker = (newDate) => {
     setDate(newDate);
   };
   const handleDropdown = (e) => {
     setDropdownValue(e.target.value);
   };
-
+  const handleSwitch = () => {
+    handleSwitchChange(!checked);
+  };
   return (
     <Grid container justifyContent="center" alignItems="center" size={12}>
       <Grid item xs={12}>
@@ -50,6 +53,10 @@ const SiteCondition = () => {
       <Grid item xs={12} padding={15} className="site-condition-container">
         <h3>Search Field Component: </h3>
         <SearchField />
+      </Grid>
+      <Grid item xs={12} padding={15} className="site-condition-container">
+        <h3>Switch: </h3>
+        <DSTSwitch checked={checked} handleChange={handleSwitch} />
       </Grid>
     </Grid>
   );
---
+++ src/pages/Footer/index.js
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>Footer Container Copyright etc.</p>
+    </div>
+  );
+};
+
+export default Footer;
