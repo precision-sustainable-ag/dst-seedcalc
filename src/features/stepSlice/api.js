b975b1ea583cff6489955493c7bcccf89f5d74b0
YazidZidane <xjt980408@gmail.com>
2023-10-12 12:01:55 -0400

    remove clgs

--- src/features/stepSlice/api.js
+++ src/features/stepSlice/api.js
@@ -72,7 +72,6 @@ export const getZoneData = createAsyncThunk(
   async ({ zip }, thunkAPI) => {
     const url = `https://phzmapi.org/${zip}.json`;
     const res = await fetch(url).then((data) => data.json());
-    console.log("response for zones", res);
     return res.zone;
   }
 );
