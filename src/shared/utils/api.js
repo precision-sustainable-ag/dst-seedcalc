c3d574f2fece13885ab424027eed9a462e40d6a7
YazidZidane <xjt980408@gmail.com>
2024-04-24 15:05:29 -0400

    move history api url and schema to env

--- src/shared/utils/api.js
+++ src/shared/utils/api.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-console */
-const historyApiUrl = 'https://develophistory.covercrop-data.org/v1';
+import { userHistoryApiUrl, userHistorySchema } from '../data/keys';
+
+const historyApiUrl = `${userHistoryApiUrl}/v1`;
 
 // TODO: two ways to handle error when fetching, another way is move these apis to redux toolkit
-
 const fetchData = async (url, options = {}) => {
   try {
     const res = await fetch(url, options);
@@ -17,28 +18,8 @@ const fetchData = async (url, options = {}) => {
   }
 };
 
-// get latest schema id
-export const getSchemaId = async (accessToken = null) => {
-  const url = `${historyApiUrl}/services/2/schema`;
-  const config = {
-    method: 'GET',
-    headers: {
-      'content-type': 'application/json',
-      Authorization: `Bearer ${accessToken}`,
-    },
-  };
-  try {
-    const data = await fetchData(url, config);
-    const schemaId = data.data.id;
-    return schemaId;
-  } catch (err) {
-    console.error('Error trying to get schema ID: ', err);
-    throw err;
-  }
-};
-
 export const postHistory = async (accessToken = null, historyData = null) => {
-  const schemaId = await getSchemaId(accessToken);
+  const schemaId = userHistorySchema;
   const url = `${historyApiUrl}/history`;
   const config = {
     method: 'POST',
@@ -64,8 +45,7 @@ export const postHistory = async (accessToken = null, historyData = null) => {
 // }
 
 export const getHistories = async (accessToken = null) => {
-  const schemaId = await getSchemaId(accessToken);
-  const url = `${historyApiUrl}/histories?schema=${schemaId}`;
+  const url = `${historyApiUrl}/histories?schema=${userHistorySchema}`;
   const config = {
     method: 'GET',
     headers: {
