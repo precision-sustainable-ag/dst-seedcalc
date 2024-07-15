1b4e8eae9fa73a1d5748dd0ef0fca11949775db1
YazidZidane <xjt980408@gmail.com>
2024-07-15 11:58:50 -0400

    create link for patchnote

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -7,6 +7,8 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
 import AuthButton from '../Auth/AuthButton';
 
+const patchNotesURL = 'https://precision-sustainable-ag.atlassian.net/wiki/spaces/DST/pages/285147137/Seeding+Rate+Calculator+Patch+Notes';
+
 const NavBar = () => {
   const [anchor, setAnchor] = useState(null);
   const open = Boolean(anchor);
@@ -33,34 +35,33 @@ const NavBar = () => {
               <MenuIcon />
             </Button>
             <Menu anchorEl={anchor} open={open} onClose={closeMenu}>
-              <MenuItem sx={{ p: '0 1rem' }}>
-                <AuthButton
-                  variant="text"
-                  type={isAuthenticated ? 'Logout' : 'Login'}
-                />
-              </MenuItem>
-              {/* <MenuItem>
+              <MenuItem onClick={() => window.open(patchNotesURL)}>
                 <Typography fontSize="0.875rem" fontWeight="bold">
-                  Profile
+                  Release Notes
                 </Typography>
-              </MenuItem> */}
+              </MenuItem>
               <MenuItem onClick={() => navigate('/feedback')}>
                 <Typography fontSize="0.875rem" fontWeight="bold">
                   Feedback
                 </Typography>
               </MenuItem>
-
+              <MenuItem sx={{ p: '0 0.5rem' }}>
+                <AuthButton
+                  variant="text"
+                  type={isAuthenticated ? 'Logout' : 'Login'}
+                />
+              </MenuItem>
             </Menu>
           </>
         )
         : (
           <>
-            {/* <Button>
+            <Button onClick={() => window.open(patchNotesURL)} sx={{ textTransform: 'none' }}>
               <Typography fontSize="0.875rem" fontWeight="bold">
-                Profile
+                Release Notes
               </Typography>
-            </Button> */}
-            <Button onClick={() => navigate('/feedback')}>
+            </Button>
+            <Button onClick={() => navigate('/feedback')} sx={{ textTransform: 'none' }}>
               <Typography fontSize="0.875rem" fontWeight="bold">
                 Feedback
               </Typography>
