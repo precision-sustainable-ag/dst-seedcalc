150f7b7c96ce7ef3393ef3f4e5409a8aee3a60f7
YazidZidane <xjt980408@gmail.com>
2024-07-17 10:11:28 -0400

    update url for release note

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -7,7 +7,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
 import AuthButton from '../Auth/AuthButton';
 
-const patchNotesURL = 'https://precision-sustainable-ag.atlassian.net/wiki/spaces/DST/pages/285147137/Seeding+Rate+Calculator+Patch+Notes';
+const patchNotesURL = 'https://precision-sustainable-ag.atlassian.net/wiki/spaces/DST/pages/285147137/Seeding+Rate+Calculator+Release+Notes';
 
 const NavBar = () => {
   const [anchor, setAnchor] = useState(null);
