db39ed706a1131ad064ab80a850f8b9574ea0270
YazidZidane <xjt980408@gmail.com>
2024-08-13 14:26:56 -0400

    add component testing
    fix a problem cy not running on firefox

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: 'http://127.0.0.1:3000/',
+  },
+
+  component: {
+    devServer: {
+      framework: 'create-react-app',
+      bundler: 'webpack',
+    },
   },
 });
