2cda38bef042d1284f8c4373bb88d737cc2a10d3
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2024-09-12 15:48:40 -0400

    change url and make it not.exist

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:80/',
+    baseUrl: 'http://localhost/',
     experimentalRunAllSpecs: true,
     trashAssetsBeforeRuns: true,
     setupNodeEvents(on, config) {
