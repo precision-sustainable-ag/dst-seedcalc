dcc3e7fd9bc1af60a626260f6e94517f5b3c6677
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2024-09-12 17:01:05 -0400

    add dev tests

---
+++ dev.cypress.config.js
@@ -0,0 +1,36 @@
+const { defineConfig } = require('cypress');
+require('dotenv').config();
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000/',
+    experimentalRunAllSpecs: true,
+    trashAssetsBeforeRuns: true,
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      // include any other plugin code...
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config;
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: 'create-react-app',
+      bundler: 'webpack',
+    },
+    numTestsKeptInMemory: 1,
+    experimentalMemoryManagement: true,
+  },
+
+  env: {
+    // Auth0
+    auth0_username: process.env.AUTH0_USERNAME,
+    auth0_password: process.env.AUTH0_PASSWORD,
+    auth0_domain: process.env.REACT_APP_AUTH0_DOMAIN,
+    auth0_audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+    auth0_client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
+  },
+});
