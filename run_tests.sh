6ff3b499366f3046c4726d4dba85f7ca0c50b67f
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2024-09-13 11:57:55 -0400

    use npm command as entrypoint

--- run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Running Cypress component tests..."
-echo "Running Cypress component tests..."
-echo "Running Cypress component tests..."
-echo "Running Cypress component tests..."
-npx cypress run --component
