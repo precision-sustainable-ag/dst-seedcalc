cd4c66ac4b7814ed3d51325bbe4639a81da49b7b
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2024-09-13 11:47:49 -0400

    use entrypoint re cypress docs

--- run_tests.sh
+++ run_tests.sh
@@ -3,3 +3,4 @@ echo "Running Cypress component tests..."
 echo "Running Cypress component tests..."
 echo "Running Cypress component tests..."
 echo "Running Cypress component tests..."
+npx cypress run --component
