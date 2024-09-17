393a7733fc3b42a8a36a29ec48067f8a955ee54e
YazidZidane <xjt980408@gmail.com>
2024-09-17 11:11:16 -0400

    update map testing, add data-tests

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -3,7 +3,8 @@ export const clickStateMap = (council) => {
   cy.intercept({ url: 'https://events.mapbox.com/**' }, { log: false });
   cy.visit('/');
   cy.get('.mapboxgl-canvas').should('be.visible');
-  cy.get('div[class^="map_loadingContainer"]', { timeout: 10000 }).should('not.exist');
+  // TODO: add data-test to loadingContainer in shared-components
+  cy.get('div[class^="_loadingContainer"]', { timeout: 10000 }).should('not.exist');
   // eslint-disable-next-line cypress/no-unnecessary-waiting
   cy.wait(1000);
   if (council === undefined) {
