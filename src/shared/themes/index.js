a0c75c84e8272d0f9e22657406d93ca7e588315f
Jingtong Xu <xjt980408@gmail.com>
2025-02-26 16:17:09 -0500

    update disabled button

--- src/shared/themes/index.js
+++ src/shared/themes/index.js
@@ -68,6 +68,11 @@ const theme = createTheme({
             textTransform: 'none',
             padding: '0.5rem',
             border: '1px solid #4F5F30',
+            '&.Mui-disabled': {
+              color: '#757575',
+              border: '1px solid #737373',
+              backgroundColor: '#F0F0F0',
+            },
           },
         },
       ],
