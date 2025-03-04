40d184e866b50982e5794f39d98eef942d5ac9d3
Jingtong Xu <xjt980408@gmail.com>
2025-03-04 07:05:41 -0500

    update species selections test

--- cypress/e2e/SpeceisSelection.cy.js
+++ cypress/e2e/SpeceisSelection.cy.js
@@ -1,14 +1,14 @@
+/* eslint-disable prefer-destructuring */
 import { mockSiteCondition } from '../support/utils';
 
 describe('Species Selection', () => {
-  const selectType = 'Brassica';
-  const selectSpecies = 'Radish, Daikon';
-
   beforeEach(() => {
     mockSiteCondition();
   });
 
   it('should be able to click on species card to select or unselect a species', () => {
+    const selectType = Cypress.env('selectTypes')[0];
+    const selectSpecies = Cypress.env('selectCrops')[0];
     cy.getByTestId(`accordion-${selectType}`).click();
     cy.getByTestId(`species-card-${selectSpecies}`).find('button').click();
     cy.getByTestId(`species-card-${selectSpecies}`).find('img')
@@ -28,6 +28,8 @@ describe('Species Selection', () => {
   });
 
   it('next button should be disabled at first, and be available after select >=1 species', () => {
+    const selectType = Cypress.env('selectTypes')[0];
+    const selectSpecies = Cypress.env('selectCrops')[0];
     cy.getByTestId('next_button').should('be.disabled');
     cy.getByTestId(`accordion-${selectType}`).click();
     cy.getByTestId(`species-card-${selectSpecies}`).find('button').click();
@@ -35,6 +37,8 @@ describe('Species Selection', () => {
   });
 
   it('should be able to unselect a species by clicking on the sidebar', () => {
+    const selectType = Cypress.env('selectTypes')[0];
+    const selectSpecies = Cypress.env('selectCrops')[0];
     cy.getByTestId(`accordion-${selectType}`).click();
     cy.getByTestId(`species-card-${selectSpecies}`).find('button').click();
     cy.getByTestId(`sidebar-${selectSpecies}`).should('be.visible');
@@ -49,32 +53,45 @@ describe('Species Selection', () => {
   });
 
   it('should be able to use the search field to search a species', () => {
-    cy.getByTestId('species-selection-search').should('be.visible').type('Radish');
+    const selectType = Cypress.env('selectTypes')[0];
+    const selectSpecies = Cypress.env('selectCrops')[0];
+    cy.getByTestId('species-selection-search').should('be.visible').type(selectSpecies);
     cy.get('[data-test^="accordion-"]').should('have.class', 'Mui-expanded');
     cy.getByTestId(`accordion-${selectType}`).getByTestId(`species-card-${selectSpecies}`).should('be.visible');
     cy.get('[data-test^="accordion-"]').get('[data-test^="species-card-"]').should('have.length', 1);
   });
 
-  it('should show the diversity bar with ratio of species', () => {
-    const grasses = ['Barley, Winter', 'Sudangrass'];
-    cy.getByTestId(`accordion-${selectType}`).click();
-    cy.getByTestId(`species-card-${selectSpecies}`).find('button').click();
-    cy.getByTestId('diversity_bar').should('be.visible');
-    cy.getByTestId('accordion-Grass').click();
-    grasses.forEach((grass) => {
-      cy.getByTestId(`species-card-${grass}`).find('button').click();
+  it.only('should show the diversity bar with ratio of species', () => {
+    const selectTypes = Cypress.env('selectTypes');
+    const selectSpecies = Cypress.env('selectCrops');
+    cy.getByTestId(`accordion-${selectTypes[0]}`).click();
+    if (selectTypes[1] !== selectTypes[0]) cy.getByTestId(`accordion-${selectTypes[1]}`).click();
+
+    selectSpecies.forEach((species) => {
+      cy.getByTestId(`species-card-${species}`).find('button').click();
     });
-    cy.getByTestId('diversity-Brassica').should('have.css', 'flex-grow', '1');
-    cy.getByTestId('diversity-Grass').should('have.css', 'flex-grow', '2');
+
+    if (selectTypes[0] !== selectTypes[1]) {
+      cy.getByTestId(`diversity-${selectTypes[0]}`).should('have.css', 'flex-grow', '1');
+      cy.getByTestId(`diversity-${selectTypes[1]}`).should('have.css', 'flex-grow', '1');
+    } else {
+      cy.getByTestId(`diversity-${selectTypes[0]}`).should('have.css', 'flex-grow', '2');
+    }
   });
 });
 
-describe('Species Selection NECCC & SCCC', () => {
-  it('should be able to select species for NECCC', () => {
+describe('Species Selection NECCC', () => {
+  beforeEach(() => {
     mockSiteCondition('NECCC');
-    const selectType = 'Brassica';
-    const selectSpecies = ['Brassica, Forage', 'Mustard'];
-    cy.getByTestId(`accordion-${selectType}`).click();
+  });
+
+  it('should be able to select species for NECCC', () => {
+    const selectTypes = Cypress.env('selectTypes');
+    const selectSpecies = Cypress.env('selectCrops');
+
+    cy.getByTestId(`accordion-${selectTypes[0]}`).click();
+    if (selectTypes[1] !== selectTypes[0]) cy.getByTestId(`accordion-${selectTypes[1]}`).click();
+
     selectSpecies.forEach((species) => {
       cy.getByTestId(`species-card-${species}`).find('button').click();
       cy.getByTestId(`species-card-${species}`).find('img')
@@ -86,12 +103,20 @@ describe('Species Selection NECCC & SCCC', () => {
         .should('be.visible');
     });
   });
+});
+
+describe('Species Selection SCCC', () => {
+  beforeEach(() => {
+    mockSiteCondition('SCCC');
+  });
 
   it('should work for SCCC', () => {
-    mockSiteCondition('SCCC');
-    const selectType = 'Grass';
-    const selectSpecies = ['Millet, Japanese', 'Sorghum-sudangrass'];
-    cy.getByTestId(`accordion-${selectType}`).click();
+    const selectTypes = Cypress.env('selectTypes');
+    const selectSpecies = Cypress.env('selectCrops');
+
+    cy.getByTestId(`accordion-${selectTypes[0]}`).click();
+    if (selectTypes[1] !== selectTypes[0]) cy.getByTestId(`accordion-${selectTypes[1]}`).click();
+
     selectSpecies.forEach((species) => {
       cy.getByTestId(`species-card-${species}`).find('button').click();
       cy.getByTestId(`species-card-${species}`).find('img')
