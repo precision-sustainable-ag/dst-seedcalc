715c3131a0f513274950c56534a7418de266cdad
Jingtong Xu <xjt980408@gmail.com>
2025-03-07 11:54:19 -0500

    fix calculator reset problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Grid } from '@mui/material';
@@ -12,34 +12,39 @@ import Feedback from './pages/Feedback/Feedback';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@fontsource/ibm-plex-sans';
 
-const App = () => (
-  <ThemeProvider theme={dstTheme}>
-    <div
-      className="App"
-      style={{
-        textAlign: 'center',
-        backgroundColor: '#fffff2',
-        minHeight: '100vh',
-        overflowX: 'hidden',
-      }}
-    >
-      <BrowserRouter>
-        <Auth0ProviderWithNavigate>
-          <Grid container justifyContent="center">
-            <Grid item xs={12} lg={8}>
-              <Header />
-              <Routes>
-                <Route path="/" element={<Calculator />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/feedback" element={<Feedback />} />
-                <Route path="/profile" element={<PSAProfile />} />
-              </Routes>
+const App = () => {
+  // initially set calculator here
+  const [calculator, setCalculator] = useState(null);
+
+  return (
+    <ThemeProvider theme={dstTheme}>
+      <div
+        className="App"
+        style={{
+          textAlign: 'center',
+          backgroundColor: '#fffff2',
+          minHeight: '100vh',
+          overflowX: 'hidden',
+        }}
+      >
+        <BrowserRouter>
+          <Auth0ProviderWithNavigate>
+            <Grid container justifyContent="center">
+              <Grid item xs={12} lg={8}>
+                <Header />
+                <Routes>
+                  <Route path="/" element={<Calculator calculator={calculator} setCalculator={setCalculator} />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/feedback" element={<Feedback />} />
+                  <Route path="/profile" element={<PSAProfile />} />
+                </Routes>
+              </Grid>
             </Grid>
-          </Grid>
-        </Auth0ProviderWithNavigate>
-      </BrowserRouter>
-    </div>
-  </ThemeProvider>
-);
+          </Auth0ProviderWithNavigate>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default App;
