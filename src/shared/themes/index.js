69bf0c46d9ace88ed8bea01596ec84caf7bcac3f
Jingtong Xu <xjt980408@gmail.com>
2025-08-06 16:28:03 -0400

    integrate shared theme

--- src/shared/themes/index.js
+++ src/shared/themes/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { createTheme } from '@mui/material/styles';
 import { deepmerge } from '@mui/utils';
 import { PSATheme } from 'shared-react-components/src';
@@ -81,71 +80,6 @@ const theme = createTheme({
   },
 });
 
-const testTheme = createTheme({
-  palette: {
-    primary: {
-      main: '#598445',
-    },
-    secondary: {
-      main: '#27739E',
-    },
-    error: {
-      main: '#DD3804',
-    },
-    text: {
-      primary: '#1F1F1F',
-      secondary: '#565656',
-    },
-    main: {
-      accent1: '#598445',
-      accent2: '#27739E',
-      background1: '#E9E6E0',
-      text: '#1F1F1F',
-    },
-    additional: {
-      background2: '#F5F5F5',
-      grey2: '#AAAAAA',
-      grey1: '#737373',
-      greydark: '#565656',
-      support1: '#416782',
-      support2: '#408D79',
-      support3: '#C48B0F',
-      support4: '#91643B',
-      support5: '#624469',
-      error: '#DD3804',
-    },
-  },
-  typography: {
-    fontFamily: [
-      '"IBM Plex Sans"',
-      '"Roboto"',
-      '"Helvetica Neue"',
-      '"Arial"',
-      'sans-serif',
-    ].join(','),
-    color: '#1F1F1F',
-    header: {
-      color: '#1F1F1F',
-      fontFamily: 'IBM Plex Sans',
-      fontSize: '2.5rem',
-      fontStyle: 'normal',
-      fontWeight: 600,
-      lineHeight: 'normal',
-    },
-    subtitle: {
-      color: '#1F1F1F',
-      fontFamily: 'IBM Plex Sans',
-      fontSize: '1.25rem',
-      fontStyle: 'italic',
-      fontWeight: 500,
-      lineHeight: 'normal',
-    },
-    button: {
-      fontWeight: 600,
-    },
-  },
-});
-
-const dstTheme = createTheme(deepmerge(theme, testTheme));
+const dstTheme = createTheme(deepmerge(theme, PSATheme));
 
 export default dstTheme;
